8174f795f2c10d847325a5c671510208d65806b9
Nguyen Van Binh <binhnv@smartosc.com>
2015-08-07 18:17:08 +0700

    [EE-1] header implement

--- skin/frontend/rwd/titany/scss/config.rb
+++ skin/frontend/rwd/titany/scss/config.rb
@@ -1,5 +1,5 @@
 # note: this should never truly be refernced since we are using relative assets
-http_path = "/skin/frontend/rwd/enterprise/"
+http_path = "/skin/frontend/rwd/titany/"
 css_dir = "../css"
 sass_dir = "../scss"
 images_dir = "../images"
